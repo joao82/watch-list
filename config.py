beb96a2603f7f4add49a76039f572a8b106a864d
joaomartins <joao82@gmail.com>
2022-12-09 17:25:09 +0200

    commit_database

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get("DATABASE_URL"))
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
+# Development Stage with SQLite database
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get("DATABASE_URL"))
 
